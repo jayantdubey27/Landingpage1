3ec8a8dae9d09c8f98794faa691a474896d51f94
jayantdubey27 <jayantdubey27@gmail.com>
2024-10-24 14:07:52 +0530

    Improving logo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <div className="flex justify-between">
         <div className="flex items-center justify-center gap-5">
-            <img src="./Resources/Logo.jpg" alt="Logo" className='h-10 w-8 md:h-28 w-28 lg:h-44 w-56'/>
+            <img src="./Resources/Logo.jpg" alt="Logo" className='h-10 w-8 md:h-28 w-28 lg:h-44 w-72'/>
             <button className='bg-gradient-to-r from-orange-400 to-red-400 text-xs text-white hover:text-neutral-500 px-2.5 py-1.5 rounded-2xl '>We are hiring</button>
         </div>
         <div>
           <ul className='hidden lg:flex justify-between items-center font-bold from-neutral-400 gap-6'>
             <li><a href=''>Plans</a></li>
-            <li><a href=''>Why Us</a></li>
+            <li><a href=''>Why Us?</a></li>
           </ul>
         </div>
         <div className='hidden lg:flex justify-center items-center font-bold gap-6'>
